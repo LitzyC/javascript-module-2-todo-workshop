7659a95a4b60d7769f3cfa185bbeb1b598974a36
litzycabrera <litzy.prinss1@gmail.com>
2022-09-13 21:43:24 +0200

    Ejercicio hasta el 6to

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,53 @@
-alert("hello!");
+const formulario = document.querySelector('#new-todo');
+const todos = []
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+const renderTodos = (todos) => {
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    todos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo))
+    })
+}
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+    // Setup the todo text
+    todoText.textContent = todo
+    containerEl.appendChild(todoText)
+
+    // Setup container
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    return todoEl
+}
+
+const createTodo = (text) => {
+    todos.push(text)
+}
+
+const evento =  (e) => {
+    e.preventDefault()
+    
+    const text = e.target.elements.text.value.trim()
+    
+    if (text.length > 0) {
+        createTodo(text)
+        e.target.elements.text.value = ''
+    }
+    console.log("Texto añadido: " + todos)
+};
+
+formulario.addEventListener('submit', evento);
+
+//let paragraph = document.createElement("lista"); 
+//myElement.appendChild(paragraph); 
+//paragraph.innerText = "How are you?";
+//let myElement = document.querySelector("#boton");
+//myElement.appendChild(generateTodoDOM);
+//generateTodoDOM.innerText = "Text agregado"; 
